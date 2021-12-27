53c6c5d2ec17cc24c1159559e79c4c060e1398ec
Mr. Leung <ling.home.worker@home.com>
2021-12-27 09:28:34 +0800

    update

--- pcb.py
+++ pcb.py
@@ -164,17 +164,16 @@ def ts(cm):
         cm.SetTimeStamp(int(datetime.datetime.now().timestamp()))
 if __name__=="__main__":
     #org='/home/arm/git/kicad/timer/timer.kicad_pcb'
-    org='/home/arm/git/pcb_esc/m48/m48.kicad_pcb'
+    org='/home/arm/git/pcb_esc/8051/8051.kicad_pcb'
     bn=bakname(org,"/home/arm/git/kicad/backup")
     print("backup to `{}`".format(bn))
     shutil.copyfile(org,bn)
     pcb=PcbInfo(org)
-    #pcb.zones()
+    pcb.zones()
     #zone(org)
     #pcb.most_item(via)
 
     #pcb.arrageItem()
     #pcb.defineEdge(30*unit,30*unit,100*unit,120*unit,int(0.15 * unit))
-    # 手工运行
-    #pcb.save()
+    pcb.save()
     
